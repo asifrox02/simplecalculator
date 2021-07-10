8e5a35b540c6cd9c6a6c00b0507bbb6cd35f4c69
asifrox02 <asifrox02@gmail.com>
2021-07-11 02:56:53 +0500

    now complete

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SimpleCalculator'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: CalcButton(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CalcButton extends StatefulWidget {
+  @override
+  _CalcButtonState createState() => _CalcButtonState();
+}
+
+class _CalcButtonState extends State<CalcButton> {
+  double _currentValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    var calc = SimpleCalculator(
+      value: _currentValue,
+      hideExpression: false,
+      hideSurroundingBorder: true,
+      onChanged: (key, value, expression) {
+        setState(() {
+          _currentValue = value;
+        });
+        print("$key\t$value\t$expression");
+      },
+      onTappedDisplay: (value, details) {
+        print("$value\t${details.globalPosition}");
+      },
+      theme: const CalculatorThemeData(
+
+      ),
+    );
+    return OutlineButton(
+      child: Text(_currentValue.toString()),
+      onPressed: () {
+        showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: calc);
+            });
+      },
+    );
+  }
+}
